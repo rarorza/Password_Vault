ac0e5b0838055f257ba54923c8d826563927d6d0
Rafael <rarorza@proton.me>
2023-06-19 14:43:14 -0300

    Update

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import random
 import string
 
-length_pwd = int(input("What is the password length? "))
-min_numbers = int(input("Minimum numbers? "))
-min_special = int(input("Minimum special characters? "))
-length_pwd -= min_numbers + min_special
 
-pwd = (
-    random.choices(string.ascii_letters, k=length_pwd)
-    + random.choices(string.digits, k=min_numbers)
-    + random.choices("!@#$%^&*", k=min_special)
-)
+def main():
+    length_pwd = int(input("Password length: "))
+    min_numbers = int(input("Minimum numbers: "))
+    min_special = int(input("Minimum special characters: "))
+    length_pwd -= min_numbers + min_special
 
-random.shuffle(pwd)
-pwd = "".join(pwd)
+    pwd = gen_pwd(length_pwd, min_numbers, min_special)
 
-print("Sua senha gerada", pwd)
+    print("Your password:", pwd)
+
+
+def gen_pwd(length_pwd, min_numbers, min_special):
+    pwd = (
+        random.choices(string.ascii_letters, k=length_pwd)
+        + random.choices(string.digits, k=min_numbers)
+        + random.choices("!@#$%^&*", k=min_special)
+    )
+    random.shuffle(pwd)
+    pwd = "".join(pwd)
+    return pwd
+
+
+if __name__ == "__main__":
+    main()
 
 # To do:
 # Avoid ambiguous characters
